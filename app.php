042d5a4413fa010f02af19e9bf62678f428ca16a
CyberRodent <fej.reblok@gmail.com>
2016-10-27 16:03:54 +0000

    Explain what this is doing

--- app.php
+++ app.php
@@ -19,6 +19,14 @@ $refresh_token = REFRESH_TOKEN;
 $e = new eveCrest($refresh_token);
 
 
+/*
+ * What this will do is return a report of the market orders (buy and/or 
+ * sell) for a region and optionally for a given station in a region.
+ * This report may be used to gague the relative supply and demand for 
+ * items.
+ */
+
+
 // These will be input arguments - hard coded for now
 $region_id = 10000042;       // Metropolis
 $location_filter = 60005686; // Hek; show only orders in this location
@@ -29,9 +37,7 @@ $market_group_id = 206 ;        // 5 is Frigates;
 $market_group_id = 1855 ;       // ice ores
 $market_group_id = 1372;        // Destroyers too, what's the difference?  This is ALL TYPES OF Dessies
 $market_group_id = 582;         // Destroyers  -  this is the standard Destroyers
-
 $market_group_id = 1857 ;       // 18 is Minerals
-
 
 
 $mgsg = new MarketGroupSummaryGenerator($e, $region_id, $market_group_id, $location_filter);
